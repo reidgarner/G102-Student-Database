62d8af017141e544f5801161234a5d6d6fb2b1f6
reidgarner <reidbgarner@gmail.com>
2018-11-27 13:00:12 -0700

    running knex seed successfully

--- migrations/20181127115258_students.js
+++ migrations/20181127115258_students.js
@@ -2,12 +2,12 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('students', (student) => {
         student.increments('ID');
-        student.string('Name');
-        student.string('Favorite Animal');
-        student.string('Previous Occupation');
-        student.decimal('Hometown Lat');
-        student.decimal('Hometown Long');
-        student.string('Useless Superpower');
+        student.string('name');
+        student.string('fave_animal');
+        student.string('previous_occupation');
+        student.decimal('hometown_lat');
+        student.decimal('hometown_long');
+        student.string('useless_superpower');
     })
 }
 
